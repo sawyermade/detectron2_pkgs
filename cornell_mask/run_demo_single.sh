e3520283d050e9b2dcc42a56c33f1918b6f9bdf8
sawyermade <sawyermade0@gmail.com>
2020-03-06 13:46:17 -0500

    lazy

--- cornell_mask/run_demo_single.sh
+++ cornell_mask/run_demo_single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 get_cornell_masks.py \
 	--input-dir outputs \
-	--output outputs_masks_crop-${1}/ \
-	--confidence-threshold 0.${1} \
-	--config-file configs/COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml \
+	--output outputs_masks_crop-${2}/ \
+	--confidence-threshold 0.${2} \
+	--config-file $1 \
 	--cornell-single
