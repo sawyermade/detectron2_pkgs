b4321c43d6623ec0f9ed64b0d9ccad9c65c8d9be
sawyermade <sawyermade0@gmail.com>
2020-03-08 17:16:56 -0400

    http done

---
+++ http/http_client_rs.py
@@ -0,0 +1,104 @@
+import os, sys, requests, pyrealsense2 as rs
+import numpy as np, cv2, jsonpickle, argparse
+
+# Arg parser
+def get_parser():
+	parser = argparse.ArgumentParser(description="Detectron2 demo for builtin models")
+	parser.add_argument(
+		'--ip',
+		dest='ip',
+		help='Server IP',
+		default='127.0.0.1',
+		type=str
+	)
+	parser.add_argument(
+		'--port',
+		dest='port',
+		help='Server Port',
+		default='665',
+		type=str
+	)
+	parser.add_argument(
+		'--width',
+		dest='width',
+		help='Image Capture Width',
+		default=640,
+		type=int
+	)
+	parser.add_argument(
+		'--height',
+		dest='height',
+		help='Image Capture height',
+		default=480,
+		type=int
+	)
+
+	return parser.parse_args()
+
+# Uploads to Detectron
+def upload(url, frame):
+	# Prep headers for http req
+	content_type = 'application/json'
+	headers = {'content_type': content_type}
+
+	# jsonpickle the numpy frame
+	_, frame_png = cv2.imencode('.png', frame)
+	frame_json = jsonpickle.encode(frame_png)
+
+	# Post and get response
+	try:
+		response = requests.post(url, data=frame_json, headers=headers)
+		if response.text:
+			# Decode response and return it
+			retList = jsonpickle.decode(response.text)
+			retList[0] = cv2.imdecode(retList[0], cv2.IMREAD_COLOR)
+			# retList[-1] = [cv2.imdecode(m, cv2.IMREAD_GRAYSCALE) for m in retList[-1]]
+			
+			# returns [vis.png, bbList, labelList, scoreList, maskList]
+			return retList
+		else:
+			return None
+	except:
+		return None
+
+def main():
+	# Arguments
+	args = get_parser()
+	url = f'http://{args.ip}:{args.port}'
+
+	# Starts captures
+	width, height = args.width, args.height
+	pipeline = rs.pipeline()
+	config = rs.config()
+	config.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
+	config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
+	profile = pipeline.start(config)
+
+	while True:
+		# Get frames
+		frames = pipeline.wait_for_frames()
+		frame = np.asanyarray(frames.get_color_frame().get_data())
+		
+		# Sends to detectron
+		# returns [vis.png, bbList, labelList, scoreList, maskList]
+		retList = upload(url, frame)
+		if not retList:
+			continue
+
+		# Shows img
+		visImg = retList[0]
+		bbList = retList[1]
+		# maskList = retList[-1]
+		# visImg[:,:,0] *= maskList[0]
+		# visImg[:,:,1] *= maskList[0]
+		# visImg[:,:,2] *= maskList[0]
+		visImg = cv2.resize(visImg, (1200, 900))
+
+		cv2.imshow('Inference', visImg)
+		k = cv2.waitKey(1)
+		if k == 27:
+			cv2.destroyAllWindows()
+			break 
+
+if __name__ == '__main__':
+	main()
--- http/http_server.py
+++ http/http_server.py
@@ -1,10 +1,138 @@
-import os, sys, cv2, numpy as np, detectron2
+import os, sys, cv2, numpy as np, argparse, multiprocessing as mp, time, pyrealsense2
+import flask, glob, time, jsonpickle, torch
+from detectron2.config import get_cfg
+from detectron2.data.detection_utils import read_image
+from detectron2.utils.logger import setup_logger
+from predictor import VisualizationDemo
 
-def parse_args():
-	pass
+def setup_cfg(args):
+	# load config from file and command-line arguments
+	cfg = get_cfg()
+	cfg.merge_from_file(args.config_file)
+	cfg.merge_from_list(args.opts)
+	# Set score_threshold for builtin models
+	cfg.MODEL.RETINANET.SCORE_THRESH_TEST = args.confidence_threshold
+	cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.confidence_threshold
+	cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = args.confidence_threshold
+	cfg.freeze()
+	return cfg
+
+def get_parser():
+	parser = argparse.ArgumentParser(description="Detectron2 demo for builtin models")
+	parser.add_argument(
+		"--config-file",
+		default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
+		metavar="FILE",
+		help="path to config file",
+	)
+	parser.add_argument(
+		"--confidence-threshold",
+		type=float,
+		default=0.5,
+		help="Minimum score for instance predictions to be shown",
+	)
+	parser.add_argument(
+		"--opts",
+		help="Modify config options using the command-line 'KEY VALUE' pairs",
+		default=[],
+		nargs=argparse.REMAINDER,
+	)
+	parser.add_argument(
+		'--cuda',
+		dest='cuda',
+		help='Enter cuda card number to use as integer',
+		default='0',
+		type=str
+	)
+	parser.add_argument(
+		'--ip',
+		dest='ip',
+		help='Server IP',
+		default='127.0.0.1',
+		type=str
+	)
+	parser.add_argument(
+		'--port',
+		dest='port',
+		help='Server Port',
+		default='665',
+		type=str
+	)
+
+	return parser.parse_args()
+
+# Globals
+args = get_parser()
+DOMAIN = args.ip
+PORT = args.port
+app = flask.Flask(__name__)
+model = None
+
+# Cuda device setup
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]=args.cuda
+
+# Awaits POST requests for inference
+@app.route('/', methods=['POST'])
+def upload_file():
+	# Get request and unzip/decode
+	r = flask.request
+	im = jsonpickle.decode(r.data)
+	im = cv2.imdecode(im, cv2.IMREAD_COLOR)
+
+	# Run inference
+	# timers = defaultdict(Timer)
+	# with c2_utils.NamedCudaScope(0):
+	# 	cls_boxes, cls_segms, cls_keyps = infer_engine.im_detect_all(
+	# 		model, im, None, timers=timers
+	# 	)
+	predictions, visualized_output = model.run_on_image(im)
+	# return flask.Response(response=None)
+
+	if "panoptic_seg" in predictions:
+		pred, segments_info = predictions["panoptic_seg"].to(torch.device('cpu'))
+	
+	else:
+		if "sem_seg" in predictions:
+			pred = predictions['sem_seg'].to(torch.device('cpu'))
+		
+		if "instances" in predictions:
+			pred = predictions["instances"].to(torch.device('cpu'))
+
+	# print(pred)
+	bbList = pred.pred_boxes.tensor.numpy()
+	labelList = pred.pred_classes.numpy()
+	maskList = pred.pred_masks.numpy()
+	scoreList = pred.scores.numpy()
+	# print(f'pred:\n{bbList}')
+
+	# # Encodes to png files
+	# pngList = [cv2.imencode('.png', m)[1] for m in maskList]
+	retList = [cv2.imencode('.png', visualized_output.get_image())[1], bbList, labelList, scoreList, maskList]
+
+	# # Encodes to jsonpickle and sends json
+	retList_encoded = jsonpickle.encode(retList)
+
+	# returns [vis.png, bbList, labelList, scoreList, maskList]
+	return flask.Response(response=retList_encoded, status=200, mimetype='application/json')
+
 
 def main():
-	args = parse_args()
+	# Globals
+	global model
+
+	# Setup logger
+	mp.set_start_method("spawn", force=True)
+	setup_logger(name="fvcore")
+	logger = setup_logger()
+	logger.info("Arguments: " + str(args))
+
+	# Setup config and predictor
+	cfg = setup_cfg(args)
+	model = VisualizationDemo(cfg)
+
+	# Run http app
+	app.run(port=PORT, host=DOMAIN, debug=False)
 
 if __name__ == '__main__':
 	main()
