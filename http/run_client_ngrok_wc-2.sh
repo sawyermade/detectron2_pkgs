5db00e13ed996f6596b76c1ebc81c19b527e2fd0
sawyermade <sawyermade0@gmail.com>
2020-04-16 10:24:13 -0400

    lazy

---
+++ http/run_client_ngrok_wc-2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 http_client_demo.py \
+	-wc \
+	-vo \
+	-cn 2 \
+	-ip $1 \
+	-p 80
---
+++ http/run_client_ngrok_wc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 http_client_demo.py \
+	-wc \
+	-vo \
+	-ip $1 \
+	-p 80
