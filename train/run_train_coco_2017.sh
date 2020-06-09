42da94439ad79f29b56d7d7b58b72cea38ed85ed
sawyermade <sawyermade0@gmail.com>
2020-06-09 07:40:18 -0400

    fixes

--- train/run_train_coco_2017.sh
+++ train/run_train_coco_2017.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+temp_cuda="0"
 if [ -n "$1" ]
 then
         temp_cuda="$1"
-else
-        temp_cuda="0"
 fi
 
+temp_config="../configs/COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"
+if [ -n "$2" ]
+then 
+	temp_config="$2"
+fi
+
+#echo "cuda: $temp_cuda"
+#echo "config: $temp_config"
+
 python3 train_coco_2017.py \
-        -cf ../configs/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml \
+        -cf $temp_config \
         -cu $temp_cuda \
         SOLVER.IMS_PER_BATCH 2 SOLVER.BASE_LR 0.0025
