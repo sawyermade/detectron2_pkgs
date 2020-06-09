2e0a39e2821d30e5b59592235c1f811cbd702f3e
sawyermade <sawyermade0@gmail.com>
2020-06-09 06:43:15 -0400

    lazy

--- train/run_train_coco_2017.sh
+++ train/run_train_coco_2017.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ -n "$1" ]
+then
+        temp_cuda="$1"
+else
+        temp_cuda="0"
+fi
+
 python3 train_coco_2017.py \
-	-cf ../configs/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml \
-	SOLVER.IMS_PER_BATCH 2 SOLVER.BASE_LR 0.0025
+        -cf ../configs/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml \
+        -cu $temp_cuda \
+        SOLVER.IMS_PER_BATCH 2 SOLVER.BASE_LR 0.0025
